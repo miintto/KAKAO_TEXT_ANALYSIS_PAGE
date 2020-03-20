66447d63a28adafbbe284d0468d5ed406cc45554
miintto <minjp93@gmail.com>
2020-03-21 02:14:54 +0900

    20200321 [CONVERTOR] enter단위로 끊어진 텍스트 연결

--- static/analysis/convertor.py
+++ static/analysis/convertor.py
@@ -13,34 +13,47 @@ class Convertor:
 	def __init__(self):
 		self.date = None
 		self.title = None
-		self.df = None
+		self.df_input = None
+		self.chat_list = list()
+
+	def _load_file(self, file):
+		self.df_input = pd.read_csv(file, sep='\n', header=None, encoding='utf-8', error_bad_lines=False)
+		self.title = self.df_input[0].values[0]
+		print(f'>>> Convertor._load_file()   - file = {file}')
+
+	def _append_chat(self):
+		for line in self.df_input[0].values:
+			try:
+				split1 = line.find('] [')
+				split2 = line.find(':')
+				name = line[1:split1]
+				time = line[(split1+3):(split2+3)].replace('오전', 'AM').replace('오후', 'PM')
+				time = dt.datetime.strftime(dt.datetime.strptime(time, "%p %I:%M"), "%H:%M")
+				chat = line[(split2+5):]
+				self.chat_list.append([self.date, name, time, chat])
+			except:
+				if '--------------- ' in line:
+					n = line.find('요일')
+					self.date = str(dt.datetime.strptime(line[:n-2], '--------------- %Y년 %m월 %d일').date())
+					pass
+				else:
+					self.chat_list.append([self.date, None, None, line])
+		print(f'>>> Convertor._append_chat() - len(chat_list) = {len(self.chat_list)}')
+
+	def _join_chat(self):
+		array = self.chat_list
+		for i in range(1, len(array))[::-1]:
+			if not array[i][1]:
+				array[i - 1][3] += '\n' + array[i][3]
+				del array[i]
+		del array[0]
+		self.chat_list = array
+		print(f'>>> Convertor._join_chat()   - len(chat_list) = {len(self.chat_list)}')
 
 	def convert(self, file):
-		self.df = pd.read_csv(file, sep='\n', header=None, encoding='utf-8', error_bad_lines=False)
-		df_concat = []
-		self.title = self.df[0].values[0]
-		for line in self.df[0].values:
-			name, time, chat = self.split_chatting(line)
-			df_concat.append([self.date, time, name, chat])
-		print(f'>>> finished convert {self.title}')
-		df_chat = pd.DataFrame(np.array(df_concat), columns=['date', 'time', 'name', 'chat'])
-		return df_chat.dropna().reset_index(drop=True)
-
-	def split_chatting(self, line):
-		try:
-			split1 = line.find('] [')
-			split2 = line.find(':')
-			name = line[1:split1]
-			time = line[(split1+3):(split2+3)].replace('오전', 'AM').replace('오후', 'PM')
-			time = dt.datetime.strftime(dt.datetime.strptime(time, "%p %I:%M"), "%H:%M")
-			chat = line[(split2+5):]
-			return name, time, chat
-		except:
-			if '--------------- ' in line:
-				n = line.find('요일')
-				self.date = str(dt.datetime.strptime(line[:n-2], '--------------- %Y년 %m월 %d일').date())
-				return None, None, None
-			else:
-				return None, None, line
-
-
+		self._load_file(file)
+		self._append_chat()
+		self._join_chat()
+		print(f'>>> Convertor finished : {self.title}')
+		df_chat = pd.DataFrame(self.chat_list, columns=['date', 'name', 'time', 'chat'])
+		return df_chat
