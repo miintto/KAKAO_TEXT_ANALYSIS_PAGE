c4638407a96dc09fb6847f8a78a1520332feafd2
Cloud User <centos@ip-172-26-5-18.ap-northeast-2.compute.internal>
2020-04-08 13:02:44 +0000

    20200408 [WSGI] apache wsgi 연동

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = '/home/centos/kakao/KAKAO_TEXT_ANALYSIS_PAGE'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
